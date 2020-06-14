399cb167ec447a63c5c039740351c870a96ab500
Kyle Walters <kyle.p.walters@gmail.com>
2020-06-14 23:06:30 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -4,3 +4,9 @@ first_number = 1
 second_number = 2
 
 sum = first_number + second_number
+
+difference = first_number - second_number
+
+product = first_number * second_number
+
+quotient = first_number / second_number
